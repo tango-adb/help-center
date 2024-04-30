0d0fd78385fd41beaf1562181929f1b385eda104
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2024-04-30 11:26:11 +0800

    add more content

--- docs/screen-mirror/_pro.tsx
+++ docs/screen-mirror/_pro.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import styles from './_pro.module.css'
+import styles from './_pro.module.css';
 
 export default function Pro() {
   return (
     <p>
       <span className={styles.pro}>Pro</span>
-      <span>This feature requires Pro plan or higher.</span>
+      <span>This feature requires Pro or a higher plan.</span>
     </p>
   );
 }
